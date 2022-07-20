7e1edab58620391d06dbcd0643b2c1929244f9c9
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2022-07-20 13:26:19 -0400

    Add passing test with SampleClass

---
+++ src/main/java/SampleClass/SampleClass.java
@@ -0,0 +1,7 @@
+package SampleClass;
+
+public class SampleClass {
+    public int addFive(int num) {
+        return num + 5;
+    }
+}
---
+++ src/test/java/tests/SampleClassTest.java
@@ -0,0 +1,25 @@
+package tests;
+
+import SampleClass.SampleClass;
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SampleClassTest {
+
+    private SampleClass sampleClass;
+
+    @Before
+    public void testInit() {
+        sampleClass = new SampleClass();
+    }
+
+    @Test
+    public void testAddFiveAddsFive() throws IOException {
+        sampleClass = new SampleClass();
+        assertEquals(7, sampleClass.addFive(2));
+    }
+
+}
