bb029c7a50399e070f83719df2815c673435d044
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2022-07-21 10:24:24 -0400

    Delete other implementations for demo purposes

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,9 +3,7 @@ import peopleAnalyzer.analyzers.AgeAggregator;
 import peopleAnalyzer.interfaces.IAnalyzer;
 import peopleAnalyzer.interfaces.IPeopleRepository;
 import peopleAnalyzer.interfaces.IReporter;
-import peopleAnalyzer.reporters.FileOutputReporter;
 import peopleAnalyzer.reporters.SystemOutputReporter;
-import peopleAnalyzer.repositories.CSVRepository;
 import peopleAnalyzer.repositories.ObjectRepository;
 
 public class App {
@@ -13,11 +11,9 @@ public class App {
         System.out.println("Starting People Analyzer");
         System.out.println("=========================================");
 
-//        IPeopleRepository repository = new CSVRepository();
         IPeopleRepository repository = new ObjectRepository();
         IAnalyzer analyzer = new AgeAggregator();
-//        IReporter reporter = new SystemOutputReporter();
-        IReporter reporter = new FileOutputReporter();
+        IReporter reporter = new SystemOutputReporter();
 
         new PeopleAnalyzer(
             repository,
